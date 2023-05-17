c0f5dd49f7a029fe430b606729e598af1dad5940
JonathanIrizarry <jonathan.irizarry3@upr.edu>
2023-05-16 21:39:32 -0400

    Updated Map.js

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const google = window.google = window.google ? window.google : {};
-
 const Map = () => {
   const mapRef = useRef(null);
   const [startLocation, setStartLocation] = useState('');
@@ -13,26 +11,29 @@ const Map = () => {
   const [travelTime, setTravelTime] = useState('');
 
   useEffect(() => {
-    // Load the Google Maps JavaScript API script
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA57_WpqmqJsDU-Fn4XpWlERxmNvxSoXTk&libraries=places`;
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
+    const loadGoogleMapsScript = () => {
+      if (!window.google) {
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA57_WpqmqJsDU-Fn4XpWlERxmNvxSoXTk&libraries=places`;
+        script.async = true;
+        script.defer = true;
+        script.addEventListener('load', initializeMap);
+        document.body.appendChild(script);
+      } else {
+        initializeMap();
+      }
+    };
 
-    // Initialize the map
-    script.onload = () => {
+    const initializeMap = () => {
       const map = new window.google.maps.Map(mapRef.current, {
-        center: { lat: 18.2075, lng: -67.1396 }, // Set the initial center of the map
-        zoom: 8, // Set the initial zoom level
+        center: { lat: 18.2075, lng: -67.1396 },
+        zoom: 8,
       });
 
-      // Create the autocomplete instance for the start location input
       const startAutocomplete = new window.google.maps.places.Autocomplete(
         document.getElementById('start-input')
       );
 
-      // Handle place selection for start location
       startAutocomplete.addListener('place_changed', () => {
         const place = startAutocomplete.getPlace();
         if (!place.geometry) {
@@ -41,12 +42,10 @@ const Map = () => {
         }
         setStartLocation(place.formatted_address);
 
-        // Clear previous start marker
         if (startMarker) {
           startMarker.setMap(null);
         }
 
-        // Set new start marker on the map
         const marker = new window.google.maps.Marker({
           position: place.geometry.location,
           map: map,
@@ -55,12 +54,10 @@ const Map = () => {
         setStartMarker(marker);
       });
 
-      // Create the autocomplete instance for the destination location input
       const destinationAutocomplete = new window.google.maps.places.Autocomplete(
         document.getElementById('destination-input')
       );
 
-      // Handle place selection for destination location
       destinationAutocomplete.addListener('place_changed', () => {
         const place = destinationAutocomplete.getPlace();
         if (!place.geometry) {
@@ -69,12 +66,10 @@ const Map = () => {
         }
         setDestinationLocation(place.formatted_address);
 
-        // Clear previous destination marker
         if (destinationMarker) {
           destinationMarker.setMap(null);
         }
 
-        // Set new destination marker on the map
         const marker = new window.google.maps.Marker({
           position: place.geometry.location,
           map: map,
@@ -83,94 +78,84 @@ const Map = () => {
         setDestinationMarker(marker);
       });
 
-      // Initialize the directions service and renderer
       const directionsService = new window.google.maps.DirectionsService();
-      const directionsRenderer = new window.google.maps.DirectionsRenderer();
+      const directionsRenderer = new window.google.maps.DirectionsRenderer({ map: map });
       setDirectionsService(directionsService);
       setDirectionsRenderer(directionsRenderer);
-      directionsRenderer.setMap(map);
     };
 
+    loadGoogleMapsScript();
+
     return () => {
-      // Clean up the script element when the component is unmounted
-      document.body.removeChild(script);
+      const googleMapScript = document.querySelector('script[src^="https://maps.googleapis.com/maps/api/js"]');
+      if (googleMapScript) {
+        googleMapScript.removeEventListener('load', initializeMap);
+        googleMapScript.parentNode.removeChild(googleMapScript);
+      }
     };
   }, []);
 
-  useEffect(() => {
-    if (!startLocation && startMarker) {
-      // Remove start marker from the map
-      startMarker.setMap(null);
-      setStartMarker(null);
-    }
-    }, [startLocation]);
-    
-    useEffect(() => {
-    if (!destinationLocation && destinationMarker) {
-    // Remove destination marker from the map
-    destinationMarker.setMap(null);
-    setDestinationMarker(null);
-    }
-    }, [destinationLocation]);
-    
-    const handleRouteButtonClick = () => {
+  const handleRouteButtonClick = () => {
     if (!startLocation || !destinationLocation) {
-    console.error('Please enter start and destination locations');
-    return;
+      console.error('Please enter start and destination locations');
+      return;
     }
+
     directionsService.route(
-    {
-    origin: startLocation,
-    destination: destinationLocation,
-    travelMode: 'DRIVING',
-    },
-    (response, status) => {
-    if (status === 'OK') {
-    directionsRenderer.setDirections(response);
-    const route = response.routes[0];
-    if (route && route.legs && route.legs.length > 0) {
-    const leg = route.legs[0];
-    const durationText = leg.duration ? leg.duration.text : '';
-    setTravelTime(durationText);
-    }
-    } else {
-    console.error('Directions request failed due to ' + status);
-    }
-    }
-    );
-    };
-    
-    return (
-    <div>
-    <div>
-    <input
-    id="start-input"
-    type="text"
-    placeholder="Start Location"
-    value={startLocation}
-    onChange={(e) => setStartLocation(e.target.value)}
-    style={{ width: '400px' }}
-    />
-    </div>
-    <div>
-    <input
-    id="destination-input"
-    type="text"
-    placeholder="Destination Location"
-    value={destinationLocation}
-    onChange={(e) => setDestinationLocation(e.target.value)}
-    style={{ width: '400px' }}
-    />
-    </div>
-    <div>
-    <button onClick={handleRouteButtonClick}>Generate Route</button>
-    </div>
-    <div>
-    <span>Travel Time: {travelTime}</span>
-    </div>
-    <div ref={mapRef} style={{ height: '600px', width: '150%' }} />
-    </div>
-    );
-    };
-    
-    export default Map;
+      {
+        origin: startLocation,
+        destination: destinationLocation,
+        travelMode: 'DRIVING',
+      },
+      (response,
+
+status) => {
+if (status === 'OK') {
+directionsRenderer.setDirections(response);
+const route = response.routes[0];
+if (route && route.legs && route.legs.length > 0) {
+const leg = route.legs[0];
+const durationText = leg.duration ? leg.duration.text : '';
+setTravelTime(durationText);
+}
+} else {
+console.error('Directions request failed due to ' + status);
+}
+}
+);
+};
+
+return (
+<div>
+<div style={{ marginBottom: '16px' }}>
+<input
+id="start-input"
+type="text"
+placeholder="Start Location"
+value={startLocation}
+onChange={(e) => setStartLocation(e.target.value)}
+style={{ width: '400px' }}
+/>
+</div>
+<div style={{ marginBottom: '16px' }}>
+<input
+id="destination-input"
+type="text"
+placeholder="Destination Location"
+value={destinationLocation}
+onChange={(e) => setDestinationLocation(e.target.value)}
+style={{ width: '400px' }}
+/>
+</div>
+<div style={{ marginBottom: '16px' }}>
+<button onClick={handleRouteButtonClick}>Generate Route</button>
+</div>
+<div style={{ marginBottom: '16px' }}>
+<span>Travel Time: {travelTime}</span>
+</div>
+<div ref={mapRef} style={{ height: '600px', width: '150%' }} />
+</div>
+);
+};
+
+export default Map;
